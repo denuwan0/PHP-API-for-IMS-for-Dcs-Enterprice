e5afcdb612ce1dbaf8fc864e48c475bec4ff234b
denuwan0 <denuwan0@gmail.com>
2024-03-03 23:55:47 +0530

    manager dashboard modify

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -94,8 +94,71 @@ class Dashboard extends CI_Controller {
 			echo json_encode($userdata);
 		}
 		else if($sys_user_group_name == "Manager"){
-			$data = $this->Dashboard_model->fetch_all();
-			echo json_encode($data->result_array());
+			
+			$yard_vehicles = 0;
+			$yard_employees = 0;
+			$customers = 0;
+			
+			$complete_rental_orders = 0;
+			$complete_retail_orders = 0;
+			$complete_online_orders = 0;
+			
+			
+			
+			$data2 = $this->Dashboard_model->fetch_all_active_yard_vehicles_count_manager($emp_branch_id);
+			$data2 = $data2->result_array();
+			
+			$data3 = $this->Dashboard_model->fetch_all_active_employee_count_manager();
+			$data3 = $data3->result_array();
+			
+			$data4 = $this->Dashboard_model->fetch_all_active_customer_count_manager();
+			$data4 = $data4->result_array();
+			
+			$data5 = $this->Dashboard_model->fetch_all_complete_retail_order_manager();
+			$data5 = $data5->result_array();
+			
+			$data6 = $this->Dashboard_model->fetch_all_complete_rental_order_manager();
+			$data6 = $data6->result_array();
+			
+			$data7 = $this->Dashboard_model->fetch_all_complete_online_order_manager();
+			$data7 = $data7->result_array();
+			
+			$data8 = $this->Dashboard_model->fetch_all_latest_orders_manager();
+			$data8 = $data8->result_array();
+			
+			$data9 = $this->Dashboard_model->fetch_all_latest_items_manager();
+			$data9 = $data9->result_array();
+			
+			$data10 = $this->Dashboard_model->fetch_all_branch_wise_sale_manager();
+			$data10 = $data10->result_array();
+			
+			$system_users = $data1[0]['user_count'];
+			$yard_vehicles = $data2[0]['yard_vehicles'];
+			$yard_employees = $data3[0]['employee_count'];
+			$customers = $data4[0]['customer_count'];
+			
+			$complete_rental_orders = $data5[0]['retail_order_count'];
+			$complete_retail_orders = $data6[0]['rental_order_count'];
+			$complete_online_orders = $data7[0]['online_order_count'];
+			
+		
+			
+			$userdata = array(
+				'system_users'  			=> "",
+				'yard_vehicles' 			=> $yard_vehicles,
+				'yard_employees' 			=> $yard_employees,
+				'customers'  				=> $customers,
+				'complete_rental_orders' 	=> $complete_rental_orders,
+				'complete_retail_orders' 	=> $complete_retail_orders,
+				'complete_online_orders'  	=> $complete_online_orders,
+				'latest_orders'				=> $data8,
+				'latest_items'				=> $data9,
+				'branch_wise_sale'				=> $data10
+			);	
+			
+						
+			
+			echo json_encode($userdata);
 		}
 		else{
 			$data = $this->Dashboard_model->fetch_all_join_by_branch_id($emp_branch_id);
